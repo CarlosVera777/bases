8ba7e1bb917358176e7cad52f9666ad60732f01b
carlosVera777 <carlosvera.777@gmail.com>
2025-01-27 18:58:38 -0500

    Componentes independientes (NotFound) - rutas por defecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
@@ -18,8 +23,17 @@ export const routes: Routes = [
     loadChildren: () => import('./contact/contact.module').then((m) => m.ContactModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'not-found',
+    //loadComponent: () => import('./shared/pages/not-found/not-found.component').then((m) => m.NotFoundComponent)
+    loadComponent: async () => {
+      const m = await import('./shared/pages/not-found/not-found.component');
+      return m.NotFoundComponent
+    }
+  },
+  {
+    path: '**',
+    //redirectTo: '',
+    //pathMatch: 'full',
+    loadComponent: () => import('./shared/pages/not-found/not-found.component').then((m) => m.NotFoundComponent)
   }
 ];
