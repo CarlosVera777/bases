ea3907e9fb8ec13c1be2941e1a1e386680f2b66f
carlosVera777 <carlosvera.777@gmail.com>
2025-01-28 18:16:37 -0500

    Renderizando informacion dinamica

--- src/app/home/components/article/article.component.ts
+++ src/app/home/components/article/article.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent  implements OnInit {
-
-  constructor() { }
+  article: IArticulo;
+  
+  constructor() { 
+    setTimeout(() => {
+      this.loadArticulo();
+    }, 2000);
+  }
 
   ngOnInit() {}
 
+  loadArticulo() {
+    const data: IArticulo = {
+      title: 'Angular [DINAMICO]',
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.' +
+      'Quae rem maxime ullam enim eveniet magni, sed quo reprehenderit' +
+       'perspiciatis assumenda!',
+      image: {
+        url: '/assets/images/angular-logo.png',
+        desc: 'logo de angular'
+      } 
+    }
+    this.article = data;
+  }
+}
+
+interface IArticulo {
+  title: string;
+  description: string;
+  image: {
+    url: string;
+    desc: string;
+  }
+  id?: string;
 }
