f68372acf4f07e1fa85f60e29936721c73f80805
carlosVera777 <carlosvera.777@gmail.com>
2025-01-29 00:42:53 -0500

    Condicionales y bucles

--- src/app/models/store.models.ts
+++ src/app/models/store.models.ts
@@ -1,5 +1,12 @@
 export namespace ModelsStore {
-    export interface IItem { }
+    export interface IItem {
+        id?: string;
+        name: string;
+        description: string;
+        price: number;
+        image?: string;
+    }
+    
     export interface IPedido { }
     export interface ICarrito { }
 }
--- src/app/store/pages/store/store.component.ts
+++ src/app/store/pages/store/store.component.ts
@@ -9,11 +9,44 @@ import { Models } from 'src/app/models/models';
 })
 export class StoreComponent  implements OnInit {
 
-  carrito: Models.Store.ICarrito;
   items: Models.Store.IItem[];
-  pedidos: Models.Store.IPedido[];
+  cargando: boolean = true;
 
-  constructor() { }
+  constructor() {
+    this.loadItems();
+  }
 
   ngOnInit() {}
+
+  loadItems() {
+    setTimeout(() => {
+      this.items = DataDemo;
+      this.cargando = false;
+      console.log('items ->', this.items);
+    }, 2000);
+  }
 }
+
+const DataDemo: Models.Store.IItem[] = [
+  {
+    id: '0001',
+    name: 'Hamburguesa',
+    description: 'Con queso, salsas, papas',
+    price: 7.50,
+    image: '/assets/images/hamburguesa.webp'
+  },
+  {
+    id: '0002',
+    name: 'Hamburguesa Especial',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero maxime asperiores ex quia, nostrum laboriosam recusandae ea eaque explicabo cupiditate iure quo omnis aliquid beatae soluta odit aliquam. Velit, voluptate.',
+    price: 9.50,
+    // image: '/assets/images/hamburguesa.webp'
+  },
+  {
+    id: '0003',
+    name: 'Hamburguesa Doble',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero maxime asperiores ex quia, nostrum laboriosam recusandae ea eaque explicabo cupiditate iure quo omnis aliquid beatae soluta odit aliquam. Velit, voluptate.',
+    price: 11.50,
+    image: '/assets/images/hamburguesa.webp'
+  }
+];
